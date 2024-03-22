d9101ed328996ed6d28cb45f80708803014d6235
GabeTheFirst <81899285+GabeTheFirst@users.noreply.github.com>
2024-03-22 15:29:06 -0500

    comments in RequestTools

--- RecNetSharp/RequestTools.cs
+++ RecNetSharp/RequestTools.cs
@@ -8,14 +8,22 @@ namespace RecNetSharp
 {
     internal static class RequestTools
     {
+        // creates a query string that's a list (array) of items with the 'Query' variable as the variable
         public static string CreateQueryArray(string Query, List<long> Items)
         {
+            // the string that eventually gets returned
             string Result = "?";
+
+            // foreac item in the list of longs (most likely id in this case)
             foreach(var Item in Items)
             {
+                // add to query string the Query=Value&
                 Result += Query + "=" + Item.ToString() + "&";
             }
+            // trim the last &
             Result = Result.TrimEnd('&');
+
+            // return the completed string
             return Result;
         }
     }
