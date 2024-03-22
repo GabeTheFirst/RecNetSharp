22f14c13b55c2e225e0f618ef9fe8f512f2f0dfb
GabeTheFirst <81899285+GabeTheFirst@users.noreply.github.com>
2024-03-22 15:27:14 -0500

    comments in AccountController

--- RecNetSharp/Controllers/Accounts.cs
+++ RecNetSharp/Controllers/Accounts.cs
@@ -11,45 +11,68 @@ namespace RecNetSharp.Controllers
     // for easier usage
     public class AccountController 
     {
+        // the instance of RecNetClient this controller instance will use
         RecNetClient Client;
+
+        // set up the controller
         public AccountController(RecNetClient C) 
         {
+            // store a reference to the RecNetClient
             Client = C;
         }
 
         public async Task<Account> GetAccountAsync(string Username)
         {
+            // get account by username from the rec room api
             return await Client.Get<Account>("accounts/?username=" + Username);
         }
 
         public async Task<Account> GetAccountAsync(long Id)
         {
+            // get account by id from the rec room api
             return await Client.Get<Account>("accounts/" + Id);
         }
 
         public async Task<List<Account>> SearchAccountsAsync(string Username)
         {
+            // searches for accounts from the rec room api
             return await Client.Get<List<Account>>("accounts/search?name=" + Username);
         }
 
         public async Task<Bio> GetBioAsync(long Id)
         {
+            // get account bio by someone's id
             return await Client.Get<Bio>("accounts/" + Id + "/bio");
         }
 
         public async Task<Bio> GetBioAsync(string Username)
         {
+            // get the account this username is for
             Account A = await GetAccountAsync(Username);
-            return await Client.Get<Bio>("accounts/" + A.accountId + "/bio");
+            // then get the bio by that account's id
+            return await GetBioAsync(A.accountId);
         }
 
         public async Task<List<Account>> GetAccountsAsync(List<long> Ids)
         {
+            // convert the longs into a Query string (such as '?id=1&id=2')
             string Query = RequestTools.CreateQueryArray("id", Ids);
+
+            // check the length to see if it should be post or get
             if(Query.Length < 2048)
+            {
+                // gets the accounts as a get request, using the Query
                 return await Client.Get<List<Account>>("accounts/bulk" + Query);
+            }
             else
+            {
+                /*
+                   this gets accounts bulk as POST, and trim the ? from the query as content so RecNet doesn't ignore it
+                   form data could be used here (that's what it really is) but it's easier to just send the modified
+                   query string, since it's the same end product
+                */
                 return await Client.Post<List<Account>>("accounts/bulk", Query.Trim('?'));
+            }
         }
     }
 }
