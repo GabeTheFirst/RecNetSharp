724a066e336dc2bbb30cefe07ab48b655eb447d6
GabeTheFirst <81899285+GabeTheFirst@users.noreply.github.com>
2024-03-22 15:53:18 -0500

    comment Room Controller

--- RecNetSharp/Controllers/Rooms.cs
+++ RecNetSharp/Controllers/Rooms.cs
@@ -9,31 +9,45 @@ namespace RecNetSharp.Controllers
 {
     public class RoomsController
     {
+        // the instance of RecNetClient this controller instance will use
         RecNetClient Client;
+
+        // set up the controller
         public RoomsController(RecNetClient C)
         {
+            // store a reference of the RecNetClient
             Client = C;
         }
 
+        // get the list of rooms by a creator's account id
         public async Task<List<Room>> GetRoomsByCreatorAsync(long AccountId)
         {
+            // get the result from RecNet
             return await Client.Get<List<Room>>("rooms/createdby/" + AccountId);
         }
 
+        // get the list of rooms by a creator's account username
         public async Task<List<Room>> GetRoomsByCreatorAsync(string Username)
         {
+            // get account by the username 
             Account A = await Client.Accounts.GetAccountAsync(Username);
+            // get rooms by the creator's id
             return await GetRoomsByCreatorAsync(A.accountId);
         }
 
+        // get the list of rooms by owned a creator's account id
         public async Task<List<Room>> GetRoomsOwnedByCreatorAsync(long AccountId)
         {
+            // get the result from RecNet
             return await Client.Get<List<Room>>("rooms/ownedby/" + AccountId);
         }
 
+        // get the list of rooms by owned a creator's account username
         public async Task<List<Room>> GetRoomsOwnedByCreatorAsync(string Username)
         {
+            // get account by the username 
             Account A = await Client.Accounts.GetAccountAsync(Username);
+            // get rooms owned by the creator's id
             return await GetRoomsOwnedByCreatorAsync(A.accountId);
         }
     }
